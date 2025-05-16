d3a07c9922bddbe6904bae3ba02d4a9a0eeac802
Pedro Castro <pcastro9876@gmail.com>
2025-05-16 17:00:50 -0300

    finalizando as validacoes

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,18 +1,26 @@
 import User from "../models/User.js";
-
+import * as Yup from "yup"; // Importando o Yup para validação de dados
 class SessionController {
 
   // Criando Login para aplicacao
   static async cadastrandoUser(req, res){
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    })
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(401).json({error: "Erro na validação dos dados"});
+    }
+
     const {email} = req.body;
 
     try{  
       let user = await User.create({email})
-      return res.status(200).json(user);
+      return res.status(201).json(user);
 
 
     }catch(err){
-      return res.status(400).json({error: "User nao encontrado"});
+      return res.status(401).json({error: "Usuario ja existe"});
     }
   }
 }
